bfa87e648eec1763298579620782d0a62773e7b5
Dawid Kostka <dpkostka@gmail.com>
2021-08-31 16:05:20 +0200

    v1.0

--- script.js
+++ script.js
@@ -6,34 +6,47 @@ const amountEl_two = document.getElementById("amount-two");
 const rateEl = document.getElementById("rate");
 const swap = document.getElementById("swap");
 
-function calculate() {
+function cryptoResponse() {
   const currency_one = currencyEl_one.value;
-  const currency_two = currencyEl_two.value;
 
   fetch(
     `http://api.coinlayer.com/api/live?access_key=e386d4f97b7f34c0b7bfb372a1c941f7#`
   )
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
-      console.log(data.target);
-      console.log(typeof data.target);
-      const crypto_one = data.rates[currency_one];
-      console.log(crypto_one);
-      rateEl.innerText = crypto_one;
+      const crypto = data.rates[currency_one];
+
+      //console.log(currency_one);
+      //console.log(crypto);
+      //amountEl_two.value = (amountEl_one.value * crypto).toFixed(2);
+
+      function fiatResponse() {
+        fetch(
+          `https://v6.exchangerate-api.com/v6/4b1fd31631c11d3ad2b157e2/latest/USD`
+        )
+          .then((res2) => res2.json())
+          .then((data2) => {
+            const currency_two = currencyEl_two.value;
+            console.log(currency_two);
+            const fiat_rate = data2.conversion_rates[currency_two];
+
+            if (currency_two != "USD") {
+              amountEl_two.value =
+                amountEl_one.value * (crypto * fiat_rate).toFixed(2);
+            } else {
+              amountEl_two.value = (amountEl_one.value * crypto).toFixed(2);
+            }
+            //amountEl_two.value = (amountEl_one.value * crypto).toFixed(2);
+            //console.log(amountEl_two.value * rate);
+          });
+      }
+
+      fiatResponse();
     });
-
-  fetch(
-    `https://v6.exchangerate-api.com/v6/4b1fd31631c11d3ad2b157e2/latest/USD`
-  )
-    .then((res2) => res2.json())
-    .then((data2) => {
-      console.log(data2);
-    });
-  //
-
-  // rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
-
-  // amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
 }
-calculate();
+
+currencyEl_one.addEventListener("change", cryptoResponse);
+amountEl_one.addEventListener("input", cryptoResponse);
+currencyEl_two.addEventListener("change", cryptoResponse);
+
+cryptoResponse();
